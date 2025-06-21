96762940447633336d4f20bbd573bcee2e495b03
akash2316y <iamakashrabha@gmail.com>
2025-06-21 19:24:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,9 +65,17 @@ def progress(current, total, message, type):
 # start command
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot, I can send you restricted content by it's post link__\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Save-Restricted-Bot")]]), reply_to_message_id=message.id)
-
+	bot.send_message(
+		message.chat.id,
+		f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot, I can send you restricted content by it's post link__\n\n{USAGE}",
+		reply_markup=InlineKeyboardMarkup([
+			[
+				InlineKeyboardButton("📢 Update Channel", url="https://t.me/UnknownBotz"),
+				InlineKeyboardButton("💬 Support Group", url="https://t.me/UnknownBotzChat")
+			]
+		]),
+		reply_to_message_id=message.id
+	)
 
 @bot.on_message(filters.text)
 def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
