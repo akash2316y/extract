dac4ba5b62bf6ed3b43ace03d96857955cefb5e6
akash2316y <iamakashrabha@gmail.com>
2025-07-08 11:23:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,12 +64,13 @@ def progress(current, total, message, type):
 
 # start command
 @bot.on_message(filters.command(["start"]))
-def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+def send_start(client, message):
     bot.send_message(
         chat_id=message.chat.id,
-        text=f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot, I can send you restricted content by its post link__\n\n{USAGE}",
+        text=f"👋 Hi **{message.from_user.mention}**, I can send you restricted content by its post link.\n\n{USAGE}",
         reply_to_message_id=message.id
     )
+	
 
 @bot.on_message(filters.text)
 def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
